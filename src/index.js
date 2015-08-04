42e00e78ac5700626706b6fdc8e455be98a7b1b9
Aliaksei Shytkin <e79eas@gmail.com>
2015-08-04 12:26:06 +0300

    Add list size

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-
 import {customUnits as cu} from 'targetprocess-mashup-helper';
 import template from './template.html';
 
 cu.add({
     id: 'add_comment',
     name: 'Add Comment',
-    template: template
+    template: template,
+    sizes: [cu.sizes.LIST]
 });
 
 import Mashup from './Mashup';
