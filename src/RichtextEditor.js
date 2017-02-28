1ddcfe76462f8fd109bf80b02ec01b2a58bd1a5b
Victor Homyakov <vkhomyackov@gmail.com>
2017-02-28 15:43:04 +0300

    Bug#148064 Fix styles according to last changes in TP; extract styles to CSS; render bubble content detached

--- src/RichtextEditor.js
+++ src/RichtextEditor.js
@@ -1,78 +1,23 @@
 import React from 'react';
-import $ from 'jquery';
+import styles from './index.css';
 
 const markdownMarker = '<!--markdown-->';
 
 export default class RichtextEditor extends React.Component {
-
     componentDidMount() {
-
-        // const dom = React.findDOMNode(this);
-
-        // if ($.ui.richeditorMarkdown) {
-
-        //     $(dom).richeditorMarkdown({
-        //         saveAction: {
-        //             available: false
-        //         },
-        //         cancelOptions: {
-        //             available: false
-        //         }
-        //     })
-        //     .richeditorMarkdown('show');
-
-        // } else {
-
-            setTimeout(() => this.refs.textarea.getDOMNode().focus(), 0);
-
-        // }
-
+        setTimeout(() => this.textarea && this.textarea.focus(), 0);
     }
 
-    // componentWillUnmount() {
-
-    //     // if ($.ui.richeditorMarkdown) {
-
-    //     //     const dom = React.findDOMNode(this);
-
-    //     //     if ($(dom).richeditorMarkdown('instance')) {
-
-    //     //         $(dom).richeditorMarkdown('destroy');
-
-    //     //     }
-
-    //     // }
-
-    // }
+    getValue() {
+        return markdownMarker + this.textarea.value;
+    }
 
     render() {
-
         return (
-            <div>
-                <textarea ref="textarea" className="i-role-textarea" style={{
-                    width: 300,
-                    height: 200
-                }} autoFocus={true} />
+            <div className={styles.textareaContainer}>
+                <textarea ref={textarea => this.textarea = textarea}
+                    className={`${styles.textarea} i-role-textarea`} autoFocus={true} />
             </div>
         );
-
-    }
-
-    get value() {
-
-        // if ($.ui.richeditorMarkdown) {
-
-        //     const dom = React.findDOMNode(this);
-
-        //     if ($(dom).richeditorMarkdown('instance')) {
-
-        //         return $(dom).richeditorMarkdown('getText');
-
-        //     }
-
-        // }
-
-        return markdownMarker + this.refs.textarea.getDOMNode().value;
-
     }
 }
