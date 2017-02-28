3270180604c18459b7bdf739695d373c31c2245e
Victor Homyakov <vkhomyackov@gmail.com>
2017-02-28 15:32:29 +0300

    Update from latest mashup boilerplate

---
+++ scripts/buildLibraryDocs.js
@@ -0,0 +1,30 @@
+const shell = require('shelljs');
+
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
+
+const mashupManifestName = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../src/manifest.json'))).Name;
+const mashupPublishName = mashupManifestName || JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'))).name;
+
+const processReadmeText = (text, publishName) => {
+    const reg = /(!\[[^\]]*\])\(([^\)]+)(?!https?:\/\/)\)/g;
+
+    return text.replace(reg, (main, img, imgPath) => {
+        return `${img}(${url.resolve(`https://github.com/TargetProcess/TP3MashupLibrary/raw/master/${publishName}/`, imgPath)})`;
+    });
+};
+
+const processReadme = (publishName) => {
+    const targetFilePath = 'docs/README.md';
+    const file = shell.ls(targetFilePath)[0];
+
+    if (!file) throw new Error(`File "${targetFilePath}" is not found`);
+
+    shell.mkdir('-p', 'build/library');
+    shell.cp('-fR', 'docs/*', 'build/library/');
+    processReadmeText(cat(file), publishName).to('build/library/README.mkd');
+    shell.rm('-rf', 'build/library/README.md');
+};
+
+processReadme(mashupPublishName);
